657f22175eef0bc14855f1a12e377029ffdfb1f9
Pushpareddy-prog <pushpareddy275@gmail.com>
2024-09-13 09:55:29 +0000

    conversion of datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict";//treat all js code as new version
+
+//Number
+//Bigint
+//Object
+
+
+console.log(typeof undefined);
+
+console.log(typeof null);
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId =144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+/* prefer let const becar var has problems for block scope and global scope*/
+let accountState;
+// accountId =2
+accountEmail="hh@google.com"
+accountPassword="21212121"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountState]);
